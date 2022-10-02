d9572c43a4f6e19b067a727f3768b3c7d2787c8e
SyuLin_1167 <21080015ms@edu.fca.ac.jp>
2022-10-03 00:32:46 +0900

    プレイヤーとステージの追加

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    float Speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKey("up"))
+        {
+        }
+        if (Input.GetKey("down"))
+        {
+        }
+        if (Input.GetKey("left"))
+        {
+        }
+        if (Input.GetKey("right"))
+        {
+        }
+    }
+}
