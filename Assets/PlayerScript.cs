680afc3505ff17419cb307da959fa4ade1e325f6
rei <rena27231@gmail.com>
2022-10-05 16:22:29 +0900

    プレイヤーの移動処理

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,30 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class Player : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
-    float Speed = 3.0f;
+    Vector3 moving;          //プレイヤーの向き
+    Rigidbody rb;               
+    public float Speed=90.0f;
+    public float Grabity=9.0f;
+    public float jumpPower=10.0f;
+    bool jumpNow;
+    float x,y,z;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //プレイヤーの重力コンポーネントを取得し、倒れないように回転を制御
+       rb=GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-     if(Input.GetKey("up"))
+        MoveControll();
+         rb.velocity=moving;
+       
+    }
+
+    void FixedUpdate()
+    {
+         MoveDir();
+         JumpGrabity();
+    }
+
+    void MoveControll()
+    {
+       x=Input.GetAxisRaw("Horizontal");
+       z=Input.GetAxisRaw("Vertical");
+       moving=new Vector3(x*Time.deltaTime,0,z*Time.deltaTime);
+       moving.Normalize();
+       moving=moving*Speed;
+    }
+
+    public void MoveDir()
+    {
+            if(Mathf.Abs(moving.x)>0.001f)
+            {
+            Quaternion rotX = Quaternion.AngleAxis(moving.x,Vector3.up);
+            rotX = Quaternion.Slerp(rb.transform.rotation, rotX, 0.1f);
+            this.transform.rotation = rotX;
+            }
+            else if(Mathf.Abs(moving.z)>0.001f)    
+            {
+            Quaternion rotZ = Quaternion.AngleAxis(moving.z-90,Vector3.up);
+            rotZ = Quaternion.Slerp(rb.transform.rotation, rotZ, 0.2f);
+            this.transform.rotation = rotZ;
+            }
+    }
+
+    void JumpControll(Collision other)
+    {
+        if(other.gameObject.CompareTag("Stage"))
         {
+            jumpNow=false;
         }
-        if (Input.GetKey("down"))
+        if (Input.GetKeyDown(KeyCode.Space)) 
         {
+		    rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+		    jumpNow = true;
         }
-        if (Input.GetKey("left"))
+    }
+
+    void JumpGrabity()
+    {
+        if(jumpNow)
         {
-        }
-        if (Input.GetKey("right"))
-        {
+            rb.AddForce(new Vector3(0,Grabity,0));
         }
     }
 }
