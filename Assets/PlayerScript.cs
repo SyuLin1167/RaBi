5ed679d72786b0301a5079e26732c67dedf55041
3939MeronPan <12s10r22rme@gmil.com>
2022-10-08 14:39:19 +0900

    シリアライズで変数をprivate化/リジッドボディのキャッシュ化

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,78 +4,71 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    Vector3 moving;          //プレイヤーの向き
-    Rigidbody rb;               
-    public float Speed=90.0f;
-    public float Grabity=9.0f;
-    public float jumpPower=200.0f;
-    bool jumpFlag=true;
-    float x,z;
+    private Rigidbody _rb;                                              //リジッドボディ           
+    [SerializeField] private float Speed=90.0f;                         //移動速度
+    [SerializeField] private float jumpPower=200.0f;                    //ジャンプ力
+    bool jumpFlag=true;                                                 //ジャンプフラグ
+    [SerializeField] private float x,z;                                 //座標
 
-    // Start is called before the first frame update
     void Start()
     {
-        //プレイヤーの重力コンポーネントを取得し、倒れないように回転を制御
-       rb=GetComponent<Rigidbody>();
+       _rb=GetComponent<Rigidbody>();                                    //リジッドボディ取得
     }
 
 
     void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag=="Stage")
+        if(other.gameObject.tag=="Stage")                               //地面についていたら
         {
-            Debug.Log("Stage");
-            jumpFlag=true;
-        }
-        
+            Debug.Log("Stage");                                         //ログを出す
+            jumpFlag=true;                                              //ジャンプフラグを立てる
+        }   
     }
-    // Update is called once per frame
+   
     void Update()
     {
         MoveControll();
         JumpControll();
-       // rb.velocity=moving;
-        
     }
 
     void FixedUpdate()
     {
-         MoveDir();
+        MoveDir();
     }
 
     void MoveControll()
     {
-      x=Input.GetAxisRaw("Horizontal")*Speed;
-      z=Input.GetAxisRaw("Vertical")*Speed; 
-      rb.AddForce(x,0,z);
+        x=Input.GetAxisRaw("Horizontal")*Speed;                         //左右キーが押されたときの値をX座標に入れる    
+        z=Input.GetAxisRaw("Vertical")*Speed;                           //上下キーが押されたときの値をY座標に入れる
+        _rb.AddForce(x,0,z);                                             //値を加える
     }
 
     public void MoveDir()
     {
-        if(Mathf.Abs(x)>0.1f)                                            //左右キーが入力されたら
+        if(Mathf.Abs(x)>0.1f)                                           //左右キーが入力されたら
         {
-        Quaternion rotX = Quaternion.AngleAxis(x+90,Vector3.up);            //最大何度まで回転するか設定
-        rotX = Quaternion.Slerp(rb.transform.rotation, rotX, 0.1f);             //徐々に回転する
-        this.transform.rotation = rotX;                                         //取得した方向を向く
+        Quaternion rotX = Quaternion.AngleAxis(x+90,Vector3.up);        //最大何度まで回転するか設定
+        rotX = Quaternion.Slerp(_rb.transform.rotation, rotX, 0.1f);     //徐々に回転する
+        this.transform.rotation = rotX;                                 //取得した方向を向く
         }
-        if(Mathf.Abs(z)>0.1f)    
+        if(Mathf.Abs(z)>0.1f)                                           //上下キーも同じように処理
         {
         Quaternion rotZ = Quaternion.AngleAxis(z,Vector3.up);
-        rotZ = Quaternion.Slerp(rb.transform.rotation, rotZ, 0.2f);
+        rotZ = Quaternion.Slerp(_rb.transform.rotation, rotZ, 0.2f);
         this.transform.rotation = rotZ;
         }
     }
 
     void JumpControll()
     {
-        if(jumpFlag)
+        if(jumpFlag)                                                    //ジャンプフラグがたっていたら
         {
-           if(Input.GetKeyDown(KeyCode.Space))
+           if(Input.GetKeyDown(KeyCode.Space))                           //スペースキーを押すと
             {
-                    Debug.Log("jump");
-                    Vector3 force=new Vector3(0.0f,jumpPower,0.0f);             //ジャンプ力を設定
-                    rb.AddForce(force,ForceMode.Impulse);                       //力を加える
-                    jumpFlag=false;                                             //フラグを消す
+                    Debug.Log("jump");                                   //ログを出す
+                    Vector3 force=new Vector3(0.0f,jumpPower,0.0f);      //ジャンプ力を設定
+                    _rb.AddForce(force,ForceMode.Impulse);                //力を加える
+                    jumpFlag=false;                                      //フラグを消す
 
             }
         }
